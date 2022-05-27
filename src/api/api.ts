f4db159e777fd515d565b3423e0b5556f8bf3965
ExoticDuck <marchenkov.n@inbox.ru>
2022-05-27 14:17:10 +0300

    fixes

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: `https://social-network.samuraijs.com/api/1.0`,
     withCredentials: true,
     headers: {
-        'API-KEY': 'c4deeac1-451b-4ef8-8dc8-a4ffeee3c9cc'
+        'API-KEY': 'a51c385e-38b4-45b2-933a-739b6104467f'
     }
 })
 
